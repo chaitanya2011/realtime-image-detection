d27bfa0e1e390ec01c08c9fec2d2e2d79d753b44
chaitanya2011 <nishagurkhude@gmail.com>
2024-06-11 12:22:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,45 @@
-function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.hide();
-  classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+noseX=0;
+noseY=0;
+difference=0;
+rightWristX=0;
+leftWristX=0;
+
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX="+noseX+"noseY="+noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+
+        console.log("leftWristX="+leftWristX+"rightWristX="+rightWristX+"difference="+difference);
+    }
 }
 
 function modelLoaded(){
-  console.log('Model Loded!');
+    console.log('PoseNet Is Initialized!');
 }
 
 function draw(){
-  image(video,0,0,300,300);
-  classifier.classify(video,gotResult);
-}
-var previous_result='';
+    background('#969A97');
 
-function gotResult(error,results){
-  if(error){
-    console.error(error);
-  }else{
-    if((results[0].confidence>0.5)&&(previous_result!=results[0].label)){
-      console.log(results);
-      previous_result=results[0].label;
-      var synth=window.speechSynthesis;
-      speak_data='Object dected is -'+results[0].label;
-      var utterThis = new SpeechSynthesisUtterance(speak_data);
-      synth.speak(utterThis);
-
-      document.getElementById("result_object_name").innerHTML=results[0].label;
-      document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
-    }
-  }
+    document.getElementById("square_side").innerHTML="Width And Height of a Square will be ="+difference+"px";
+    fill('#F90093');
+    stroke('#F900093');
+    square(noseX,noseY,difference);
 }
+
